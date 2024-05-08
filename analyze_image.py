d31ff359a052ee1a9ae767afdcc1f510c69fbc22
rakthuraga <thuraga@usc.edu>
2024-05-07 20:07:28 -0700

    load model

--- analyze_image.py
+++ analyze_image.py
@@ -11,3 +11,8 @@ api_key = os.getenv('fill in with your openai api key')
 
 client = OpenAI(api_key=api_key)
 
+# Initialize OpenAI API key
+
+# Load the pre-trained model from the local directory
+model_dir = '/Users/sumalathaodati/Desktop/rhomanApi/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8/saved_model'
+model = tf.saved_model.load(model_dir)
