e8bee2e7d1eacd86767e01f07c0a9197818f2a74
rakthuraga <thuraga@usc.edu>
2024-05-09 20:19:37 -0700

    edited objects to things encountered in city environment

--- analyze_image.py
+++ analyze_image.py
@@ -48,21 +48,20 @@ def draw_bounding_boxes(image_np, detections):
 # Function to analyze objects
 def analyze_objects(scores, classes):
     labels = {
-         1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane',
-        6: 'bus', 7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light',
-        11: 'fire hydrant', 13: 'stop sign', 14: 'parking meter', 15: 'bench',
-        16: 'bird', 17: 'cat', 18: 'dog', 19: 'horse', 20: 'sheep',
-        21: 'cow', 22: 'elephant', 23: 'bear', 24: 'zebra', 25: 'giraffe',
-        27: 'backpack', 28: 'umbrella', 31: 'handbag', 32: 'tie', 33: 'suitcase',
-        34: 'frisbee', 35: 'skis', 36: 'snowboard', 37: 'sports ball', 38: 'kite',
-        39: 'baseball bat', 40: 'baseball glove', 41: 'skateboard', 42: 'surfboard',
-        43: 'tennis racket', 44: 'bottle', 46: 'wine glass', 47: 'cup', 48: 'fork',
-        49: 'knife', 50: 'spoon', 51: 'bowl', 52: 'banana', 53: 'apple',
-        54: 'sandwich', 55: 'orange', 56: 'broccoli', 57: 'carrot', 58: 'hot dog',
-        59: 'pizza', 60: 'donut', 61: 'cake', 62: 'chair', 63: 'couch',
-        64: 'potted plant', 65: 'bed', 67: 'dining table', 70: 'toilet', 72: 'tv',
-        73: 'laptop', 74: 'mouse', 75: 'remote', 76: 'keyboard', 77: 'cell phone',
-        78: 'microwave', 79: 'oven', 80: 'toaster', 81: 'sink', 82: 'refrigerator',
-        84: 'book', 85: 'clock', 86: 'vase', 87: 'scissors', 88: 'teddy bear',
-        89: 'hair drier', 90: 'toothbrush'
-    }
+    1: 'pedestrian', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'aircraft',
+    6: 'bus', 7: 'subway train', 8: 'delivery truck', 9: 'boat', 10: 'traffic signal',
+    11: 'fire hydrant', 13: 'stop sign', 14: 'parking meter', 15: 'public bench',
+    16: 'bird', 17: 'stray cat', 18: 'stray dog', 19: 'mounted police', 20: 'street vendor',
+    21: 'urban cow', 22: 'advertisement billboard', 23: 'police barricade', 24: 'road construction sign', 25: 'pedestrian bridge',
+    27: 'backpack', 28: 'umbrella', 31: 'briefcase', 32: 'tie', 33: 'rolling suitcase',
+    34: 'street performer', 35: 'skateboarder', 36: 'snowboarder', 37: 'street vendor cart', 38: 'kite',
+    39: 'street sign', 40: 'security camera', 41: 'electric scooter', 42: 'drone',
+    43: 'construction worker', 44: 'bottle', 46: 'coffee cup', 47: 'takeout cup', 48: 'fork',
+    49: 'knife', 50: 'spoon', 51: 'food container', 52: 'banana', 53: 'apple',
+    54: 'sandwich', 55: 'orange', 56: 'broccoli', 57: 'carrot', 58: 'hot dog vendor',
+    59: 'pizza slice', 60: 'donut', 61: 'cake', 62: 'public chair', 63: 'couch',
+    64: 'potted plant', 65: 'bed', 67: 'dining table', 70: 'public restroom', 72: 'billboard',
+    73: 'laptop', 74: 'mouse', 75: 'remote', 76: 'keyboard', 77: 'smartphone',
+    78: 'microwave', 79: 'oven', 80: 'toaster', 81: 'sink', 82: 'vending machine',
+    84: 'newspaper', 85: 'clock', 86: 'flower pot', 87: 'scissors',
+}
