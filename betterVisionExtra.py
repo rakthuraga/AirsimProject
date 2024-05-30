6f07cec1b38e8175e5c4045f42b6cb9186a870dd
rakthuraga <thuraga@usc.edu>
2024-05-30 00:10:15 -0700

    colors class

--- betterVisionExtra.py
+++ betterVisionExtra.py
@@ -76,3 +76,10 @@ def extract_python_code(content):
         return full_code
     else:
         return None
+    
+class colors: # You may need to change color settings
+    RED = "\033[31m"
+    ENDC = "\033[m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
