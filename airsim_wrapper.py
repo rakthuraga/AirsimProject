c5fac324f17dc6300fbf3b628f530eb018fe8e32
rakthuraga <thuraga@usc.edu>
2024-06-03 01:06:50 -0700

    airsim_wrapper.py

--- airsim_wrapper.py
+++ airsim_wrapper.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 class AirSimWrapper:
     def __init__(self):
         ip = os.getenv('PX4_SIM_HOST_ADDR', '192.168.64.1')  # Change IP if necessary
-        
+        port = int(os.getenv('PX4_SIM_PORT', '41451'))
         logger.info(f"Connecting to AirSim server at {ip}:{port}")
         self.client = airsim.VehicleClient(ip=ip, port=port)
         try:
