c4f69f1c3690034216f6f12228057c27427a8449
rakthuraga <thuraga@usc.edu>
2024-06-11 01:23:31 -0700

    wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -4,3 +4,16 @@ import logging
 import math
 
 logger = logging.getLogger(__name__)
+
+class AirSimWrapper:
+    def __init__(self):
+        ip = os.getenv('PX4_SIM_HOST_ADDR', '192.168.64.1')  # Change IP if necessary
+        port = int(os.getenv('PX4_SIM_PORT', '41451'))
+        logger.info(f"Connecting to AirSim server at {ip}:{port}")
+        self.client = airsim.VehicleClient(ip=ip, port=port)
+        try:
+            self.client.confirmConnection()
+            logger.info("Connection to AirSim successful!")
+        except Exception as e:
+            logger.error(f"Failed to connect to AirSim: {e}")
+            raise
