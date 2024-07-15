dd84276d19bfe3bdcb50cbb992fad834ddff3be6
rakthuraga <thuraga@usc.edu>
2024-07-15 13:30:35 -0700

    nltk data

--- bomboOrig.py
+++ bomboOrig.py
@@ -9,6 +9,8 @@ import re
 # OpenAI API Key
 api_key = "fill-in-with-your-api-key"
 
+# This function checks if the necessary NLTK data packages are installed.
+# If a package is missing, it will automatically download it.
 def check_and_download_nltk_data():
     try:
         nltk.data.find('tokenizers/punkt')
