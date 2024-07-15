262c5b64d47373b83a0137ab435dbcd983c918c5
rakthuraga <thuraga@usc.edu>
2024-07-15 13:30:22 -0700

    nltk data

--- bomboOrig.py
+++ bomboOrig.py
@@ -8,3 +8,19 @@ import re
 
 # OpenAI API Key
 api_key = "fill-in-with-your-api-key"
+
+def check_and_download_nltk_data():
+    try:
+        nltk.data.find('tokenizers/punkt')
+    except LookupError:
+        nltk.download('punkt')
+
+    try:
+        nltk.data.find('taggers/averaged_perceptron_tagger')
+    except LookupError:
+        nltk.download('averaged_perceptron_tagger')
+
+    try:
+        nltk.data.find('chunkers/maxent_ne_chunker')
+    except LookupError:
+        nltk.download('maxent_ne_chunker')
