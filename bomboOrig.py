c135cd11889e3b40b847ca0d40ff1ce9f13a192d
rakthuraga <thuraga@usc.edu>
2024-07-18 13:36:39 -0700

    encode image function

--- bomboOrig.py
+++ bomboOrig.py
@@ -39,3 +39,8 @@ def check_and_download_nltk_data():
 
 # Ensure NLTK data is available
 check_and_download_nltk_data()
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
